c69bb656b624c6c848edcf4e9fa0a5f0c2e5e85e
fisalas040-tech <fisalas040@gmail.com>
2026-06-12 09:57:42 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,1 +1,4 @@
-
+// Service Worker for TIH Trading Hub
+const CACHE_NAME = 'tih-v1';
+self.addEventListener('install', e => e.waitUntil(caches.open(CACHE_NAME)));
+self.addEventListener('fetch', e => e.respondWith(fetch(e.request).catch(() => caches.match(e.request))));
